c4a00f5ef8d661b3273ed299011e521ce18f8073
Jefry Morera <jefrymoreraespinoza@gmail.com>
2024-06-08 15:47:09 -0600

    footer, header, tipo de letra

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("toggleButton").addEventListener("click", function() {
-    var scrollContainer = document.getElementById("scrollContainer");
-    if (scrollContainer.style.left === "0px") {
-        scrollContainer.style.left = "-270px"; // Ocultar el menú
-    } else {
-        scrollContainer.style.left = "0px"; // Mostrar el menú
-    }
-});
